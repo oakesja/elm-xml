30f225e6372c8bb8dbae78299da3135b5d04615b
Jacob Oakes <jacoboakes92@gmail.com>
2016-07-03 10:34:57 -0400

    Added more primitive decoders and a list decoder

--- src/Xml/Decode.elm
+++ src/Xml/Decode.elm
@@ -1,7 +1,18 @@
-module Xml.Decode exposing (decodeString, (:=), string)
+module Xml.Decode
+    exposing
+        ( decodeString
+        , (:=)
+        , string
+        , int
+        , float
+        , bool
+        , empty
+        , list
+        )
 
-import Combine exposing (Parser, parse, many, between, map)
-import Combine.Char exposing (noneOf)
+import Combine exposing (Parser, parse, many, between, map, andThen, succeed, or, skipMany)
+import Combine.Char exposing (noneOf, space)
+import Combine.Num
 import String
 
 
@@ -13,7 +24,14 @@ decodeString : Decoder a -> String -> Result String a
 decodeString decoder string =
     parse decoder string
         |> fst
-        |> Result.formatError (\e -> "Failed to parse" ++ string)
+        |> Result.formatError
+            (\e ->
+                let
+                    _ =
+                        Debug.log "error" e
+                in
+                    "Failed to parse" ++ string
+            )
 
 
 (:=) : String -> Decoder a -> Decoder a
@@ -25,6 +43,42 @@ string : Decoder String
 string =
     map String.fromList
         <| many (noneOf [ '<', '>' ])
+
+
+int : Decoder Int
+int =
+    Combine.Num.int
+
+
+float : Decoder Float
+float =
+    Combine.Num.float
+
+
+bool : Decoder Bool
+bool =
+    true `or` false
+
+
+empty : a -> Decoder a
+empty value =
+    succeed value
+
+
+list : Decoder a -> Decoder (List a)
+list decoder =
+    many
+        <| between (skipMany space) (skipMany space) decoder
+
+
+true : Decoder Bool
+true =
+    Combine.string "true" `andThen` (\_ -> succeed True)
+
+
+false : Decoder Bool
+false =
+    Combine.string "false" `andThen` (\_ -> succeed False)
 
 
 startTag : Decoder String -> Decoder String
--- test/Test/Xml/Decode.elm
+++ test/Test/Xml/Decode.elm
@@ -9,6 +9,11 @@ tests : Test
 tests =
     suite "Xml.Decode"
         [ stringTest
+        , intTest
+        , floatTest
+        , boolTest
+        , emptyTest
+        , listTest
         ]
 
 
@@ -18,4 +23,67 @@ stringTest =
         [ test "valid string"
             <| assertEqual (Ok "value")
             <| decodeString ("key" := string) "<key>value</key>"
+        , test "valid string"
+            <| assertEqual (Ok "a string with multiple words")
+            <| decodeString ("key" := string) "<key>a string with multiple words</key>"
+        ]
+
+
+intTest : Test
+intTest =
+    suite "int"
+        [ test "positive int"
+            <| assertEqual (Ok 123)
+            <| decodeString ("key" := int) "<key>123</key>"
+        , test "negative int"
+            <| assertEqual (Ok -123)
+            <| decodeString ("key" := int) "<key>-123</key>"
+        ]
+
+
+floatTest : Test
+floatTest =
+    suite "float"
+        [ test "positive float"
+            <| assertEqual (Ok 1.23)
+            <| decodeString ("key" := float) "<key>1.23</key>"
+        , test "negative float"
+            <| assertEqual (Ok -1.23)
+            <| decodeString ("key" := float) "<key>-1.23</key>"
+        ]
+
+
+boolTest : Test
+boolTest =
+    suite "bool"
+        [ test "for true"
+            <| assertEqual (Ok True)
+            <| decodeString ("key" := bool) "<key>true</key>"
+        , test "for false"
+            <| assertEqual (Ok False)
+            <| decodeString ("key" := bool) "<key>false</key>"
+        ]
+
+
+emptyTest : Test
+emptyTest =
+    suite "empty"
+        [ test "when there is nothing between the start and end tag"
+            <| assertEqual (Ok 123)
+            <| decodeString ("key" := empty 123) "<key></key>"
+        , test "when there is only a self-closing tag"
+            <| assertEqual (Ok 123)
+            <| decodeString ("key" := empty 123) "<key/>"
+        ]
+
+
+listTest : Test
+listTest =
+    suite "list"
+        [ test "valid list"
+            <| assertEqual (Ok [ 1, 2, 3 ])
+            <| decodeString ("key" := list int) "<key>1 2 3</key>"
+        , test "valid list with weird spacing"
+            <| assertEqual (Ok [ 1, 2, 3 ])
+            <| decodeString ("key" := list int) "<key>  1  2  3    </key>"
         ]
